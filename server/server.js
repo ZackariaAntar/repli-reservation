4e959ffeadff9a80fc851b4ed4a3b51d83760248
Zackaria Antar <antarzack@gmail.com>
2023-10-22 12:06:58 -0500

    sourced in weddingRouter and assigned /api/wedding endpoint on server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const guestInfoRouter = require('./routes/guest.info.router')
+const weddingRouter = require('./routes/wedding.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/guest_info', guestInfoRouter);
+app.use('/api/wedding', weddingRouter);
 
 // Serve static files
 app.use(express.static('build'));
